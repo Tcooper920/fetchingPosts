9aed05478f3066ae49ad8c1946387cac9b0f710f
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2020-10-11 12:57:24 -0400

    Fetch all users.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 /* Fetching Posts & Displaying Comments */
 
+// Get all users
+const getAllUsers = async () => {
+	const getUsers = await fetch(`https://jsonplaceholder.typicode.com/users`);
+	const retrievedUsers = await getUsers.json();
+
+	return retrievedUsers;
+};
+
