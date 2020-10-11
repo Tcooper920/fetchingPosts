7bde92e248a4563d80dc907a183d6f33a7a91244
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2020-10-11 13:07:38 -0400

    Display post titles, post bodies, and post authors.

--- index.js
+++ index.js
@@ -15,3 +15,33 @@ const getAllPosts = async () => {
 
 	return retrievedPosts;
 };
+
+const displayNamePostAndTitle = async () => {
+	const users = await getAllUsers();
+	const posts = await getAllPosts();
+
+	// Display the title, post, and user on the page
+	const displayPostAndUser = (matchFound) => {
+	const postContainer = document.getElementsByClassName("posts")[0];
+
+		const post = posts.find((post) => {
+			return post.userId === matchFound;
+		});
+
+		const user = users.find((user) => {
+			return user.id === matchFound;
+		});
+
+		postContainer.innerHTML += `<div class='post-block' id='${post.id}'><strong>Post Title:</strong> ${post.title}<br>
+									<strong>Post:</strong> ${post.body}<br>
+									<span class='authors-name'><strong>Author's Name:</strong> ${user.name}</span><br>
+									<strong>Post Id:</strong> ${post.id}<br></div>`;
+	};
+	
+	// Grab and display the first post that appears in the list for each of the users
+	users.forEach((user) => {
+		displayPostAndUser(user.id);
+	});
+};
+
+displayNamePostAndTitle();
